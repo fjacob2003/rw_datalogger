7fc7f2d69c8b6ad2b542529c3cd21bcdaa1e37aa
fjacob2003 <34507734+fjacob2003@users.noreply.github.com>
2017-12-30 19:47:49 +0100

    add BT cmd

--- Datalogger_fj.ino
+++ Datalogger_fj.ino
@@ -151,46 +151,45 @@ String dht22_humidity;
 uint16_t lux;
 String bh_lux;
 float loop_dly;
-int rain;
 
 
 void setup() {
-  int rc;
+	int rc;
    
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600); //console
-  while (!Serial) { ; } //wait for usb ready
+	// Open serial communications and wait for port to open:
+	Serial.begin(9600); //console
+	while (!Serial) { ; } //wait for usb ready
    
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(chipSelect)) {
-    Serial.println("SD card access failed");
-    return;
-  } else {
-    Serial.println("card initialized.");
-  }
+	Serial.print("Initializing SD card...");
+	if (!SD.begin(chipSelect)) {
+		Serial.println("SD card access failed");
+		return;
+	} else {
+		Serial.println("card initialized.");
+	}
 
   
-  Wire.begin();
-  DS3231_init(DS3231_INTCN);
-  memset(recv, 0, BUFF_MAX);
-  getTime();
-  printTime();
+	Wire.begin();
+	DS3231_init(DS3231_INTCN);
+	memset(recv, 0, BUFF_MAX);
+	getTime();
+	printTime();
 
   logname=String(t.year,DEC)+String(t.mon,DEC)+String(t.mday,DEC)+".xls"; //WARNING:name of file must be 12 car max
   
-  Serial.print("logname : ");
-  Serial.println(logname);
+	Serial.print("logname : ");
+	Serial.println(logname);
   
-  Serial.println("Removing existing log file...");  // delete existing log with same name
-  SD.remove(logname);
+	Serial.println("Removing existing log file...");  // delete existing log with same name
+	SD.remove(logname);
   
-  Serial1.begin(9600); //serial interface to bluetooth
+	Serial1.begin(9600); //serial interface to bluetooth
 
-  //warning: to get proper address of bmp280, need to edit the adafruit lib header file
-  if (!bmp.begin()) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
-  while (1);
-  }
+	//warning: to get proper address of bmp280, need to edit the adafruit lib header file
+	if (!bmp.begin()) {
+		Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+	while (1);
+	}
 
   dht.begin();
   lightMeter.begin();
@@ -198,32 +197,32 @@ void setup() {
 }
 
 void loop() {
-  int rc;
-  getTime();
-  String dataString = String(t.hour,DEC) + '\t' + String(t.min,DEC) + '\t' + String(t.sec,DEC) + '\t'; // time stamp to logged data
-  sensorData=readSensors();
-  dataString += sensorData;  //add sensor logged data
+	int rc;
+	getTime();
+	String dataString = String(t.hour,DEC) + '\t' + String(t.min,DEC) + '\t' + String(t.sec,DEC) + '\t'; // time stamp to logged data
+	sensorData=readSensors();
+	dataString += sensorData;  //add sensor logged data
   
   printData(); //print data to serial screen
   
   logname=String(t.year,DEC)+String(t.mon,DEC)+String(t.mday,DEC)+".XLS"; //WARNING:name of file must be 12 car max //regenerate the file name in case i'm on next day
   
-  File dataFile = SD.open(logname, FILE_WRITE);  //open log file
+	File dataFile = SD.open(logname, FILE_WRITE);  //open log file
 
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-  } else {
-    Serial.println("error opening log file in write");
-  }
+	if (dataFile) {
+		dataFile.println(dataString);
+		dataFile.close();
+	} else {
+		Serial.println("error opening log file in write");
+	}
 
-  readConsole();
-  executeCommand();
+	readConsole();
+	executeCommand();
 
-  int dly_sec=60;  
+  int dly_sec=60;
   int dly_min=10;
-  for (int j=1;j<= dly_min;j++) {      // dly_min
-     for (int i=1;i<= dly_sec;i++) {   //1 min
+  for (int j=0;j<= dly_min;j++) {      // dly_min
+     for (int i=0;i<= dly_sec;i++) {   //1 min
        delay(1000);                    //1 sec
      }
   }  
@@ -234,29 +233,29 @@ void loop() {
 
 
 void getTime(){
-  DS3231_get(&t);
-  snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", t.year,
+	DS3231_get(&t);
+	snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", t.year,
              t.mon, t.mday, t.hour, t.min, t.sec);
 
 }
 
 void printTime(){
-  Serial.print("year = ");
-  Serial.print(t.year);
-  Serial.print(" month = ");
-  Serial.print(t.mon);
-  Serial.print(" day = ");
-  Serial.println(t.mday);
+	Serial.print("year = ");
+	Serial.print(t.year);
+	Serial.print(" month = ");
+	Serial.print(t.mon);
+	Serial.print(" day = ");
+	Serial.println(t.mday);
 
   Serial.print(" day of year = ");
   Serial.println(t.yday);
 
-  Serial.print("hour = ");
-  Serial.print(t.hour);
-  Serial.print(" minute = ");
-  Serial.print(t.min);
-  Serial.print(" second = ");
-  Serial.println(t.sec);
+	Serial.print("hour = ");
+	Serial.print(t.hour);
+	Serial.print(" minute = ");
+	Serial.print(t.min);
+	Serial.print(" second = ");
+	Serial.println(t.sec);
 }
 
 void printData(){
@@ -266,68 +265,68 @@ void printData(){
   Serial.print(t.min);
   Serial.print(" sec = ");
   Serial.print(t.sec);
-  Serial.print(" bmp280 temp = ");
+  Serial.print(" bmp280 temp x 100 = ");
   Serial.print(bmp280_temp);
   Serial.print(" bmp280 press = ");
   Serial.print(bmp280_press);
   Serial.print(" bmp280 alt = ");
   Serial.print(bmp280_alt);
+  Serial.print(" dht22 temp x 100 = ");
+  Serial.print(dht22_temp);
   Serial.print(" dht22 humidity = ");
   Serial.print(dht22_humidity);
   Serial.print(" bh1750 light = ");
   Serial.print(bh_lux);
-  Serial.print(" rain = ");
-  Serial.print(rain);
   Serial.println(" ");
 }
 
 void readConsole(){
-  while (Serial.available()) {
-    delay(3);
-    char c = Serial.read();
-    readstring += c;
-  }
+	while (Serial.available()) {
+		delay(3);
+		char c = Serial.read();
+		readstring += c;
+	}
 } 
 
 void executeCommand(){ 
   //dump SD to serial screen
-  if (readstring == "dump") {       
-    readstring=""; //i need to clear readstring, else i keep executing command
-    File dataFile1 = SD.open(logname);
-    if (dataFile1) { 
-      while (dataFile1.available()) {
-        Serial.println(dataFile1.read());
-      }
-      dataFile1.close();
-    } else {
-      Serial.println("error opening log file in read");
-    }
-  }
+	if (readstring == "dump") {       
+		readstring=""; //i need to clear readstring, else i keep executing command
+		File dataFile1 = SD.open(logname);
+		if (dataFile1) { 
+			while (dataFile1.available()) {
+				Serial.println(dataFile1.read());
+			}
+			dataFile1.close();
+		} else {
+			Serial.println("error opening log file in read");
+		}
+	}
 
   // remove current log file from SD card   
-  if (readstring == "clear") {
-    readstring="";
-    Serial.println("Removing existing log file...");   // delete existing log
-    SD.remove(logname);
-  }
+	if (readstring == "clear") {
+		readstring="";
+		Serial.println("Removing existing log file...");   // delete existing log
+		SD.remove(logname);
+	}
 
   //dump data to bluetooth
-  if (readstring == "blue") {
-    readstring="";
-    File dataFile1 = SD.open(logname);
-    if (dataFile1) {
-      Serial.println("Bluetooth xfer started");
-      while (dataFile1.available()) {
-        Serial1.println(dataFile1.read());   // dump data using bluetooth
-        delay(100);  //i need a delay here, else bluetooth connection drops
-      }
-      dataFile1.close();
-      Serial.println("Bluetooth xfer ended");
-    } else {
-      Serial.println("error opening log file in read");
-    }
-    delay(20);
-  }
+	if (readstring == "blue") {
+		readstring="";
+		File dataFile1 = SD.open(logname);
+		if (dataFile1) {
+			Serial.println("Bluetooth xfer started");
+			while (dataFile1.available()) {
+				Serial1.println(dataFile1.read());   // dump data using bluetooth
+				delay(100);  //i need a delay here, else bluetooth connection drops
+			}
+			dataFile1.close();
+			Serial.println("Bluetooth xfer ended");
+		} else {
+			Serial.println("error opening log file in read");
+		}
+		delay(20);
+	}
 
   //set time to hardcoded values
   if (readstring == "settime") {
@@ -359,13 +358,11 @@ String readSensors() {
 
  
 
-  bmp280_temp=String(float(bmp.readTemperature()));   // bmp280 temperature in Celsius 
+  bmp280_temp=String(int(bmp.readTemperature()*100));   // bmp280 temperature in Celsius multiply by 100 to have decimals
   bmp280_press=String(long(bmp.readPressure()));        // bmp280 pressure in pascal.i need a long here, else truncated because int is 16 bits
   bmp280_alt=String(int(bmp.readAltitude(1013.25)));    // remove decimals altitude in meter
 
   sensors_event_t event;  
-  //this measure is not good
-  /*
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
     Serial.println("Error reading temperature from DHT22");
@@ -373,23 +370,20 @@ String readSensors() {
   else {
     dht22_temp=String(int(event.temperature)*100);      // dht22 temperature in Celsius multiply by 100 to have decimals
   }
-  */
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
     Serial.println("Error reading humidity from DHT22");
   }
   else {
-    dht22_humidity=String(float(event.relative_humidity));  // dht22 rel humidity 
+    dht22_humidity=String(int(event.relative_humidity)*100);  // dht22 rel humidity multiply by 100 to have decimals
   }
 
   lux = lightMeter.readLightLevel();
   bh_lux = String(lux);
-
-  rain = analogRead(A7);
    
-  String dataString = bmp280_temp + '\t' + bmp280_press + '\t' + bmp280_alt + '\t' + dht22_humidity + '\t' + bh_lux + '\t' + rain ;
+	String dataString = bmp280_temp + '\t' + bmp280_press + '\t' + bmp280_alt + '\t' + dht22_temp + '\t' + dht22_humidity + '\t' + bh_lux ;
  
  
-  return dataString;
+	return dataString;
 }
 
